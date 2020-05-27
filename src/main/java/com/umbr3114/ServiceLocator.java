aa699e18407bd037b15174fd48a4dd4d0b56f851
Cody <chuzarski@gmail.com>
2020-05-27 18:34:41 -0400

    Added a new method to connect to the database. Checkout dbconnection.properties, as it is a template. Documentation will be updated to reflect how this file should be filled out. The Gradle build file now automates copying it to the resources directory. A new class was added to load credentials from this configuration file, using environment variables as a fallback. The rest of the codebase was updated to use this class. DatabaseCredentials was removed

--- src/main/java/com/umbr3114/ServiceLocator.java
+++ src/main/java/com/umbr3114/ServiceLocator.java
@@ -2,7 +2,7 @@ package com.umbr3114;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
-import com.umbr3114.common.DatabaseCredentials;
+import com.umbr3114.common.DatabaseConnectionDetails;
 import com.umbr3114.data.MongoClientFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,19 +28,18 @@ public class ServiceLocator {
     }
 
     private void initializeDbService() {
-        DatabaseCredentials mongoCredentials;
+        DatabaseConnectionDetails mongoConnectionDetails;
 
         try {
-            mongoCredentials = DatabaseCredentials.fromEnvironment();
+             mongoConnectionDetails = new DatabaseConnectionDetails();
         } catch (IllegalArgumentException e) {
-            // todo retry this with a file-based approach
             log.error("Database credentials are missing - cannot continue!");
             throw new IllegalStateException();
         }
-
         mongoClient = MongoClientFactory.create(
-                mongoCredentials.getUsername(),
-                mongoCredentials.getPassword()
+                mongoConnectionDetails.getUsername(),
+                mongoConnectionDetails.getPassword(),
+                mongoConnectionDetails.getHost()
         );
 
         dbService = mongoClient.getDatabase("umbrella-data");
