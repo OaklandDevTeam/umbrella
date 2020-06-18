3fea7d90c2693fa3058ad78793f764d2b037a075
klayton smith <motoklay@gmail.com>
2020-06-18 15:17:55 -0400

    yeah

--- frontend/src/components/FavoriteDropsCard.vue
+++ frontend/src/components/FavoriteDropsCard.vue
@@ -1,53 +1,53 @@
 <template>
-    <div class="user-card">
-        <ul class="subscription-list">
-            <li v-for="drop in subscribedDrops" v-bind:key="drop.dropName">
-                <u-flat-button @click.native="handleOpenDrop(drop.drop_id)" >{{drop.drop_name}}</u-flat-button>
-            </li>
-        </ul>
-    </div>
+  <div class="user-card">
+    <ul class="subscription-list">
+      <li v-for="drop in subscribedDrops" v-bind:key="drop.dropName">
+        <u-flat-button @click.native="handleOpenDrop(drop.drop_id)">{{drop.drop_name}}</u-flat-button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import store from "../store"
+import store from "../store";
 export default {
-    name: "FavoriteDropsSidebarComponent",
-    data() {
-        return {
-            subscriptions: {}
-        }
+  name: "FavoriteDropsSidebarComponent",
+  data() {
+    return {
+      subscriptions: {}
+    };
+  },
+  methods: {
+    loadUserSubscriptions() {
+      this.axios.get("/user/subscribed").then(response => {
+        store.commit("setUserSubscriptions", response.data);
+      });
     },
-    methods: {
-        loadUserSubscriptions() {
-            this.axios.get('/user/subscribed').then(response => {
-                store.commit("setUserSubscriptions", response.data)
-            })
-        },
-        handleOpenDrop(dropId) {
-            this.axios.get(`/drops/${dropId}`).then(response => {
-                this.openDrop(response.data)
-            })
-        },
-        openDrop(dropObject) {
-            store.commit("setDrop", dropObject)
-            store.commit("setPage", "drop")
-        }
+    handleOpenDrop(dropId) {
+      this.axios.get(`/drops/${dropId}`).then(response => {
+        this.openDrop(response.data);
+      });
     },
-    mounted: function() {
-            this.loadUserSubscriptions()
-    },
-    computed: {
-        subscribedDrops() {
-            return store.state.userSubscriptions;
-        }
+    openDrop(dropObject) {
+      store.commit("setDrop", dropObject);
+      store.commit("setPage", "drop");
     }
-}
+  },
+  mounted: function() {
+    this.loadUserSubscriptions();
+  },
+  computed: {
+    subscribedDrops() {
+      return store.state.userSubscriptions;
+    }
+  }
+};
 </script>
 
 <style scoped>
-    .subscription-list {
-        list-style: none;
-        list-style-position: inside;
-        padding: 0;
-    }
+.subscription-list {
+  list-style: none;
+  list-style-position: inside;
+  padding: 0;
+}
 </style>
